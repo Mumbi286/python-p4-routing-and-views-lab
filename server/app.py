1ad3e036404f2ad87fe910af8085cabecf766751
Christine Mumbi <christinemumbi286@gmail.com>
2025-12-16 15:26:44 +0300

    Adds flask routing and view lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<parameter>")
+def print_parameter(parameter):
+    print(parameter)
+    return parameter
+
+@app.route("/count/<parameter>")
+def count(parameter):
+    result = ""
+    for i in range(int(parameter)):
+        result += f"{i}\n"
+    return result
+@app.route("/math/<int:num1>/<operator>/<int:num2>")
+def math(num1, operator, num2):
+    if operator == "+":
+        return str(num1 + num2)
+    elif operator == "-":
+        return str(num1 - num2)
+    elif operator == "*":
+        return str(num1 * num2)
+    elif operator == "div":
+        return str(num1 / num2)
+    elif operator == "%":
+        return str(num1 % num2)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
